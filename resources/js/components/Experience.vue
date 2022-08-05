067b0e1c931de20f0f27dc2492a68c87df9a8ef1
rafael schebuk <rafel.schebuk@gmail.com>
2022-08-05 03:08:26 -0300

    adicionado cetificado tdd

--- resources/js/components/Experience.vue
+++ resources/js/components/Experience.vue
@@ -46,110 +46,116 @@ export default {
   props: ["content"],
   components: {
   },
-  data: () => ({"academico": [
-              {
-                "year": "01/2020 - 06/2022",
-                "title": "Ensino Superior Completo",
-                "content": "<b>Análise e desenvolvimento de sistemas</b> <br />Faculdade Estácio de Sá"
-              },
-              {
-                "year": "2001",
-                "title": "Programação em linguagens HTML e Javascript",
-                "content": "Seed Informática"
-              },
-              {
-                "year": "2001",
-                "title": "Lógica de Programação ",
-                "content": "Tekno Software"
-              },
-              {
-                "year": "2001",
-                "title": "Introdução a Banco de Dados Oracle",
-                "content": "Tekno Software"
-              }
-            ],
-            "profissional": [
-              {
-                "year": "02/05/2022 - 18/07/2022",
-                "title": "Programador de Sistemas de Informação<br/>CI&T Software",
-                "content": "Desenvolvimento Fullstack com PHP, Drupal, Lando, Docker, Mysql, Jquery, Jenkins, GitFlow <br /><b>Tecnologias</b><br />MySQL Scrum, SQL, PHP, Drupal, Jenkins, GitFlow, Jira"
-              },
-              {
-                "year": "26/06/2021 - 13/03/2022",
-                "title": "Desenvolvedor PHP Sênior<br />Meta",
-                "content": "Programação em PHP utilizando Laravel, Cakephp 3, Mysql, MongoDB, Postgresql e Programação em node.js utilizando express e front em react e vue.js, utilização de Docker<br /><b>Tecnologias</b>MySQL Scrum, SQL, Node.js, GitFlow, Azure DevOps, AWS Deployment, Laravel"
-              },
-              {
-                "year": "06/03/2021 - 13/06/2021",
-                "title": "Programador Back End<br />Alias Tecnologia",
-                "content": "Desenvolvimento e manutenção do app SpTaxi utilizando, node.js, express, mongodb, redis, docker, reactjs<br /><b>Tecnologias</b>MongoDB, Docker, GitFlow, Kanban, Redis"
-              },
-              {
-                "year": "25/01/2021 - 05/03/2021",
-                "title": "Desenvolvedor Back End Pleno<br />Núcleo Produz",
-                "content": "Programação em PHP utilizando yii2 e mysql<br/><b>Tecnologias</b><br/>MySQL, Yii"
-              },
-              {
-                "year": "04/11/2019 - 06/08/2020",
-                "title": "Programador Backend<br/ >Econet Editora Empresarial",
-                "content": "Programação em PHP utilizando Laravel, Mysql e MongoDB para criação de API's para serem consumidas por alguns sistemas e ferramentas do site e também trabalhei um pouco com o Front em VueJs<br /><b>Tecnologias</b><br />MySQL, Git, Laravel, Vue.js"
-              },
-              {
-                "year": "03/08/2016 - 22/11/2016",
-                "title": "Programador Backend<br/>Cappucino Agência Digital",
-                "content": "Programação em PHP e Javascript utilizando symfony (1.4 / 2.8), Jquery , utilização de apis via Json (Facebook, Twitter), Bancos de dados Mysql.<br /><b>Tecnologias</b><br />PHP, JavaScript, MySQL, jQuery,,APIs,JSON"
-              },
-              {
-                "year": "05/05/2016 - 18/08/2016",
-                "title": "PHP Developer<br/>Smarty Solutions",
-                "content": "Programação em PHP e Javascript utilizando CakePHP, JQuery, Bootstrap e MySQL, e também robôs de web crawling em Node.js e configuração de servidor AWS(tanto Ubuntu quanto Amazon Linux) também utilizando Docker e Cloud9.<br /><b>Tecnologias</b><br />PHP, JavaScript, MySQL, Docker, Linux, jQuery, Ubuntu, CakePHP, Web Crawling, Bootstrap"
-              },
-              {
-                "year": "23/11/2015 - 04/04/2016",
-                "title": "Programador PHP Pleno<br/>Nobre Financeira",
-                "content": "Desenvolvimento e manutenção de sistema de controle e gestão de call center em PHP tanto estruturado quanto utilizando framework Laravel com utilização de Jquery, node.js, angular, composer, git(Bitbucket) com utilização de banco de dados mysql com integração com pabx em asterisk e utilização de websockets.<br/><b>Tecnologias</b><br />PHP, MySQL, Git, jQuery, BitBucket, Composer, WebSockets, Laravel, Asterisk"
-              },
-              {
-                "year": "10/11/2015 - 04/04/2016",
-                "title": "Analista Programador PHP<br />Work Telemarketing",
-                "content": "Desenvolvimento e manutenção de sistemas em PHP utilizando frameworks Symfony 2 e Zend Framework 2 com utilização de Jquery, node.js, grunt, composer, git(Bitbucket) com utilização de bancos mysql e oracle.<br/><b>Tecnologias</b><br />PHP, MySQL, Git, jQuery, BitBucket, Grunt, Composer, Symfony"
-              },
-              {
-                "year": "10/03/2014 - 23/06/2014",
-                "title": "Programador PHP<br/>Trupe Agência Criativa",
-                "content": "Desenvolvimento de sites com Html/Css(Tableless) e com programação em PHP com o framework Zend e utilizando Ajax e Jquery<br/><b>Tecnologias</b><br />PHP, AJAX, jQuery, HTML, CSS"
-              },
-              {
-                "year": "13/06/2013 - 03/09/2013",
-                "title": "Programador PHP Pleno<br/>Jumper Technology Company",
-                "content": "Desenvolvimento de sites com Html/Css(Tableless) e com programação em PHP utilizando Ajax e Jquery<br /><b>Tecnologias</b><br />JavaScript, HTML, CSS"
-              },
-              {
-                "year": "12/09/2011 - 22/08/2012",
-                "title": "Desenvolvedor I - Resource IT Solutions",
-                "content": "Criação e alteração de páginas web tanto no portal e na intranet do banco Santander com HTML, CSS e Javascript<br/><b>Tecnologias</b><br/>JavaScript, HTML, CSS"
-              },
-              {
-                "year": "20/03/2011 - 10/05/2011",
-                "title": "Desenvolvedor PHP Junior<br/>Datto",
-                "content": "Desenvolvimento de sistemas para web com programação em PHP (orientado a objetos), AJAX, Jquery, com banco de dados em MySQL e utilizando o framework ZEND<br /><b>Tecnologias</b><br />PHP, MySQL, AJAX, jQuery"
-              },
-              {
-                "year": "01/02/2011 - 17/03/2011",
-                "title": "Assistente de Criação<br/>Cyber1 do Brasil",
-                "content": "Desenvolvimento de páginas web em HTML, CSS, Javascript com programação em PHP (estruturado) com banco de dados em MySQL<br /><b>Tecnologias</b><br />PHP, MySQL, JavaScript, HTML, CSS"
-              },
-              {
-                "year": "23/03/2008 - 17/08/2008",
-                "title": "Programador<br/>Estúdio 23",
-                "content": "Desenvolvimento de páginas web em HTML, CSS, Javascript com programação em PHP(estruturado) com banco de dados em MySQL com utilização do pacote Adobe CS3 e desenvolvimento de banners animados em Flash e Actionscript.<br/><b>Tecnologias</b><br/>PHP, MySQL, JavaScript, HTML, CSS"
-              },
-              {
-                "year": "01/02/2001 - 01/12/2003",
-                "title": "Programador Junior <br/> SCHK Sistemas",
-                "content": "Codificação e validação de programas de computador voltados para comércio eletrônico utilizando as ferramentas de Programação (Photoshop, Javascript, HTML, Flash e Actionscript) e acesso aos Bancos de Dados (SQL Server).<br/><b>Tecnologias</b><br/>SQL SERVER, JavaScript, HTML, CSS"
-              }
-            ],
+  data: () => ({
+    "academico": [
+        {
+        "year": "01/2020 - 06/2022",
+        "title": "Ensino Superior Completo",
+        "content": "<b>Análise e desenvolvimento de sistemas</b> <br />Faculdade Estácio de Sá"
+        },
+        {
+        "year": "2022",
+        "title": "Testes unitários e TDD com PHP e PHPUnit",
+        "content": "Udemy<br /> <a href='https://www.udemy.com/certificate/UC-f859d03d-0a91-4462-800a-fe858c3539a9/' target='_blank'>Certificado</a>"
+        },
+        {
+        "year": "2001",
+        "title": "Programação em linguagens HTML e Javascript",
+        "content": "Seed Informática"
+        },
+        {
+        "year": "2001",
+        "title": "Lógica de Programação ",
+        "content": "Tekno Software"
+        },
+        {
+        "year": "2001",
+        "title": "Introdução a Banco de Dados Oracle",
+        "content": "Tekno Software"
+        }
+    ],
+    "profissional": [
+        {
+        "year": "02/05/2022 - 18/07/2022",
+        "title": "Programador de Sistemas de Informação<br/>CI&T Software",
+        "content": "Desenvolvimento Fullstack com PHP, Drupal, Lando, Docker, Mysql, Jquery, Jenkins, GitFlow <br /><b>Tecnologias</b><br />MySQL Scrum, SQL, PHP, Drupal, Jenkins, GitFlow, Jira"
+        },
+        {
+        "year": "26/06/2021 - 13/03/2022",
+        "title": "Desenvolvedor PHP Sênior<br />Meta",
+        "content": "Programação em PHP utilizando Laravel, Cakephp 3, Mysql, MongoDB, Postgresql e Programação em node.js utilizando express e front em react e vue.js, utilização de Docker<br /><b>Tecnologias</b>MySQL Scrum, SQL, Node.js, GitFlow, Azure DevOps, AWS Deployment, Laravel"
+        },
+        {
+        "year": "06/03/2021 - 13/06/2021",
+        "title": "Programador Back End<br />Alias Tecnologia",
+        "content": "Desenvolvimento e manutenção do app SpTaxi utilizando, node.js, express, mongodb, redis, docker, reactjs<br /><b>Tecnologias</b>MongoDB, Docker, GitFlow, Kanban, Redis"
+        },
+        {
+        "year": "25/01/2021 - 05/03/2021",
+        "title": "Desenvolvedor Back End Pleno<br />Núcleo Produz",
+        "content": "Programação em PHP utilizando yii2 e mysql<br/><b>Tecnologias</b><br/>MySQL, Yii"
+        },
+        {
+        "year": "04/11/2019 - 06/08/2020",
+        "title": "Programador Backend<br/ >Econet Editora Empresarial",
+        "content": "Programação em PHP utilizando Laravel, Mysql e MongoDB para criação de API's para serem consumidas por alguns sistemas e ferramentas do site e também trabalhei um pouco com o Front em VueJs<br /><b>Tecnologias</b><br />MySQL, Git, Laravel, Vue.js"
+        },
+        {
+        "year": "03/08/2016 - 22/11/2016",
+        "title": "Programador Backend<br/>Cappucino Agência Digital",
+        "content": "Programação em PHP e Javascript utilizando symfony (1.4 / 2.8), Jquery , utilização de apis via Json (Facebook, Twitter), Bancos de dados Mysql.<br /><b>Tecnologias</b><br />PHP, JavaScript, MySQL, jQuery,,APIs,JSON"
+        },
+        {
+        "year": "05/05/2016 - 18/08/2016",
+        "title": "PHP Developer<br/>Smarty Solutions",
+        "content": "Programação em PHP e Javascript utilizando CakePHP, JQuery, Bootstrap e MySQL, e também robôs de web crawling em Node.js e configuração de servidor AWS(tanto Ubuntu quanto Amazon Linux) também utilizando Docker e Cloud9.<br /><b>Tecnologias</b><br />PHP, JavaScript, MySQL, Docker, Linux, jQuery, Ubuntu, CakePHP, Web Crawling, Bootstrap"
+        },
+        {
+        "year": "23/11/2015 - 04/04/2016",
+        "title": "Programador PHP Pleno<br/>Nobre Financeira",
+        "content": "Desenvolvimento e manutenção de sistema de controle e gestão de call center em PHP tanto estruturado quanto utilizando framework Laravel com utilização de Jquery, node.js, angular, composer, git(Bitbucket) com utilização de banco de dados mysql com integração com pabx em asterisk e utilização de websockets.<br/><b>Tecnologias</b><br />PHP, MySQL, Git, jQuery, BitBucket, Composer, WebSockets, Laravel, Asterisk"
+        },
+        {
+        "year": "10/11/2015 - 04/04/2016",
+        "title": "Analista Programador PHP<br />Work Telemarketing",
+        "content": "Desenvolvimento e manutenção de sistemas em PHP utilizando frameworks Symfony 2 e Zend Framework 2 com utilização de Jquery, node.js, grunt, composer, git(Bitbucket) com utilização de bancos mysql e oracle.<br/><b>Tecnologias</b><br />PHP, MySQL, Git, jQuery, BitBucket, Grunt, Composer, Symfony"
+        },
+        {
+        "year": "10/03/2014 - 23/06/2014",
+        "title": "Programador PHP<br/>Trupe Agência Criativa",
+        "content": "Desenvolvimento de sites com Html/Css(Tableless) e com programação em PHP com o framework Zend e utilizando Ajax e Jquery<br/><b>Tecnologias</b><br />PHP, AJAX, jQuery, HTML, CSS"
+        },
+        {
+        "year": "13/06/2013 - 03/09/2013",
+        "title": "Programador PHP Pleno<br/>Jumper Technology Company",
+        "content": "Desenvolvimento de sites com Html/Css(Tableless) e com programação em PHP utilizando Ajax e Jquery<br /><b>Tecnologias</b><br />JavaScript, HTML, CSS"
+        },
+        {
+        "year": "12/09/2011 - 22/08/2012",
+        "title": "Desenvolvedor I - Resource IT Solutions",
+        "content": "Criação e alteração de páginas web tanto no portal e na intranet do banco Santander com HTML, CSS e Javascript<br/><b>Tecnologias</b><br/>JavaScript, HTML, CSS"
+        },
+        {
+        "year": "20/03/2011 - 10/05/2011",
+        "title": "Desenvolvedor PHP Junior<br/>Datto",
+        "content": "Desenvolvimento de sistemas para web com programação em PHP (orientado a objetos), AJAX, Jquery, com banco de dados em MySQL e utilizando o framework ZEND<br /><b>Tecnologias</b><br />PHP, MySQL, AJAX, jQuery"
+        },
+        {
+        "year": "01/02/2011 - 17/03/2011",
+        "title": "Assistente de Criação<br/>Cyber1 do Brasil",
+        "content": "Desenvolvimento de páginas web em HTML, CSS, Javascript com programação em PHP (estruturado) com banco de dados em MySQL<br /><b>Tecnologias</b><br />PHP, MySQL, JavaScript, HTML, CSS"
+        },
+        {
+        "year": "23/03/2008 - 17/08/2008",
+        "title": "Programador<br/>Estúdio 23",
+        "content": "Desenvolvimento de páginas web em HTML, CSS, Javascript com programação em PHP(estruturado) com banco de dados em MySQL com utilização do pacote Adobe CS3 e desenvolvimento de banners animados em Flash e Actionscript.<br/><b>Tecnologias</b><br/>PHP, MySQL, JavaScript, HTML, CSS"
+        },
+        {
+        "year": "01/02/2001 - 01/12/2003",
+        "title": "Programador Junior <br/> SCHK Sistemas",
+        "content": "Codificação e validação de programas de computador voltados para comércio eletrônico utilizando as ferramentas de Programação (Photoshop, Javascript, HTML, Flash e Actionscript) e acesso aos Bancos de Dados (SQL Server).<br/><b>Tecnologias</b><br/>SQL SERVER, JavaScript, HTML, CSS"
+        }
+    ],
   })
 };
 </script>
