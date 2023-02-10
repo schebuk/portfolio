54700b8467ba262845d4a6d0858e119c9bb3018a
Rafael Schebuk <rafael.schebuk@gmail.com>
2023-02-10 09:20:46 -0300

    alterado telefone

--- resources/js/components/PersonnalCard.vue
+++ resources/js/components/PersonnalCard.vue
@@ -2,7 +2,7 @@
   <div class="personal">
     <h3>Rafael Gomes Schebuk</h3>
     <div class="data"><strong>E-mail:</strong> rafael.schebuk@gmail.com</div>
-    <div class="data"><strong>Telefone:</strong> +55 (41) 99979-4586</div>
+    <div class="data"><strong>Telefone:</strong> +55 (11) 98114-8558</div>
     <div class="data"><strong>Cidade:</strong> São Paulo - São Paulo - Brasil</div>
     <div class="data"><strong>Languages:</strong> Portugues e Ingles</div>
     <div class="data"><strong>Curriculo:</strong>
